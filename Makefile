91d1213686ad996fd860e23db49f6a1910021160
kristaps <>
2015-03-29 11:13:06 +0000

    Privileges and principals.

--- Makefile
+++ Makefile
@@ -4,15 +4,28 @@
 CALDIR		 = /tmp
 
 # You probably don't want to change anything after this.
-SRCS		 = main.c test-ical.c test-caldav.c caldav.c ical.c buf.c config.c
-OBJS		 = caldav.o ical.o buf.o md5.o config.o
-ALLOBJS		 = main.o test-ical.o test-caldav.o $(OBJS)
+BINS		 = kcaldav \
+		   test-ical \
+		   test-caldav \
+		   test-auth \
+		   test-config
+TESTSRCS 	 = test-ical.c \
+		   test-caldav.c \
+		   test-auth.c \
+		   test-config.c
+TESTOBJS 	 = test-ical.o \
+		   test-caldav.o \
+		   test-auth.o \
+		   test-config..
+SRCS		 = $(TESTSRCS) main.c caldav.c ical.c buf.c config.c principal.c auth.c
+OBJS		 = caldav.o ical.o buf.o md5.o config.o principal.o auth.o
+ALLOBJS		 = $(TESTOBJS) main.o $(OBJS)
 VERSIONS	 = version_0_0_4.xml
 VERSION		 = 0.0.4
 CFLAGS 		+= -g -W -Wall -Wstrict-prototypes -Wno-unused-parameter -Wwrite-strings
 CFLAGS		+= -DCALDIR=\"$(CALDIR)\"
 
-all: kcaldav test-ical test-caldav
+all: $(BINS)
 
 www: index.html kcaldav.tgz kcaldav.tgz.sha512
 
@@ -43,8 +56,14 @@ kcaldav: main.o $(OBJS)
 test-ical: test-ical.o $(OBJS)
 	$(CC) -o $@ test-ical.o $(OBJS) -lexpat
 
+test-auth: test-auth.o $(OBJS)
+	$(CC) -o $@ test-auth.o $(OBJS) -lexpat
+
 test-caldav: test-caldav.o $(OBJS)
 	$(CC) -o $@ test-caldav.o $(OBJS) -lexpat
+
+test-config: test-config.o $(OBJS)
+	$(CC) -o $@ test-config.o $(OBJS) -lexpat
 
 $(ALLOBJS): extern.h md5.h
 
@@ -52,6 +71,6 @@ index.html: index.xml $(VERSIONS)
 	sblg -t index.xml -o- $(VERSIONS) | sed "s!@VERSION@!$(VERSION)!g" >$@
 
 clean:
-	rm -f $(ALLOBJS) kcaldav test-ical test-caldav
-	rm -rf kcaldav.dSYM test-ical.dSYM test-caldav.dSYM
+	rm -f $(ALLOBJS) $(BINS)
+	rm -rf *.dSYM
 	rm -f index.html kcaldav.tgz kcaldav.tgz.sha512
---
+++ test-auth.c
@@ -0,0 +1,85 @@
+/*	$Id$ */
+/*
+ * Copyright (c) 2015 Kristaps Dzonsons <kristaps@bsd.lv>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "extern.h"
+
+int
+main(int argc, char *argv[])
+{
+	int		 fd, c;
+	struct stat	 st;
+	size_t		 sz;
+	char		*map, *buf;
+	struct httpauth	*p = NULL;
+
+	if (-1 != (c = getopt(argc, argv, "")))
+		return(EXIT_FAILURE);
+
+	argc -= optind;
+	argv += optind;
+
+	if (0 == argc)
+		return(EXIT_FAILURE);
+
+	if (-1 == (fd = open(argv[0], O_RDONLY, 0))) {
+		perror(argv[0]);
+		return(EXIT_FAILURE);
+	} else if (-1 == fstat(fd, &st)) {
+		perror(argv[0]);
+		close(fd);
+		return(EXIT_FAILURE);
+	} 
+
+	sz = st.st_size;
+	map = mmap(NULL, sz, PROT_READ, MAP_SHARED, fd, 0);
+	close(fd);
+
+	if (MAP_FAILED == map) {
+		perror(argv[0]);
+		return(EXIT_FAILURE);
+	} 
+
+	buf = malloc(sz + 1);
+	memcpy(buf, map, sz);
+	buf[sz] = '\0';
+	munmap(map, sz);
+	
+	if (NULL != (p = httpauth_parse(buf))) {
+		printf("username: %s\n",
+			NULL == p->user ? "(none)" : p->user);
+		printf("realm: %s\n",
+			NULL == p->realm ? "(none)" : p->realm);
+		printf("nonce: %s\n",
+			NULL == p->nonce ? "(none)" : p->nonce);
+		printf("response: %s\n",
+			NULL == p->response ? "(none)" : p->response);
+		printf("uri: %s\n",
+			NULL == p->uri ? "(none)" : p->uri);
+	}
+
+	httpauth_free(p);
+	free(buf);
+	return(NULL == p ? EXIT_FAILURE : EXIT_SUCCESS);
+}
