2fec675da7e966d643d69d339dff8be47dcc5f1f
kristapsdz <kristaps@bsd.lv>
2020-07-13 08:25:11 +0000

    Significantly update code-base: don't try to recover errors (just err() out, as recovery is just freeing up memory), use style(9), move argument safety checks to front of code, clean up user-upgrade conditional to be easier to read.

--- kcaldav.passwd.c
+++ kcaldav.passwd.c
@@ -1,6 +1,6 @@
 /*	$Id$ */
 /*
- * Copyright (c) 2015 Kristaps Dzonsons <kristaps@bsd.lv>
+ * Copyright (c) 2015, 2020 Kristaps Dzonsons <kristaps@bsd.lv>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -47,101 +47,91 @@ int verbose = 0;
 
 /*
  * See http_safe_string() in util.c.
- * This isn't included directly because it's in the kcaldav front-end library.
+ * This isn't included directly because it's in the kcaldav front-end
+ * library.
+ * It verifies non-empty strings against RFC 3986, section 3.3.
  */
 static int
 check_safe_string(const char *cp)
 {
 
-	if ('\0' == *cp)
-		return(0);
-	else if (0 == strcmp(cp, ".") || 0 == strcmp(cp, ".."))
-		return(0);
+	if (*cp == '\0')
+		return 0;
 
-	for (; '\0' != *cp; cp++)
+	if (strcmp(cp, ".") == 0 || strcmp(cp, "..") == 0)
+		return 0;
+
+	for (; *cp != '\0'; cp++)
 		switch (*cp) {
-		case ('.'):
-		case ('-'):
-		case ('_'):
-		case ('~'):
-		case ('!'):
-		case ('$'):
-		case ('('):
-		case (')'):
-		case ('*'):
-		case ('+'):
-		case (','):
-		case (';'):
-		case ('='):
-		case (':'):
-		case ('@'):
+		case '.':
+		case '-':
+		case '_':
+		case '~':
+		case '!':
+		case '$':
+		case '(':
+		case ')':
+		case '*':
+		case '+':
+		case ',':
+		case ';':
+		case '=':
+		case ':':
+		case '@':
 			break;
 		default:
-			if (isalnum(*cp))
+			if (isalnum((unsigned char)*cp))
 				break;
-			return(0);
+			return 0;
 		}
 
-	return(1);
+	return 1;
 }
 
 /*
  * Read the file "name" into the returned buffer.
- * Set "url" to be a pointer to the file-name 
+ * The file is guaranteed to be a proper iCalendar file.
  */
 static char *
 read_whole_file(const char *name)
 {
 	int		 fd;
-	char		*p;
-	void		*pp;
+	char		*p = NULL;
 	char		 buf[BUFSIZ];
 	ssize_t		 ssz;
-	size_t		 sz;
-	struct ical	*ical;
+	size_t		 sz = 0;
+	struct ical	*ical = NULL;
 
 	/*
 	 * Open the file readonly and read it in BUFSIZ at a time til
 	 * we've read the whole file.
-	 * Nil-terminate always.
+	 * NUL-terminate always, although we might read in NUL bytes
+	 * (these are filtered in ical_parse()).
 	 */
-	if (-1 == (fd = open(name, O_RDONLY, 0))) {
-		perror(name);
-		return(NULL);
-	}
-	ical = NULL;
-	p = NULL;
-	sz = 0;
+
+	if (-1 == (fd = open(name, O_RDONLY, 0)))
+		err(1, "%s", name);
+
 	while ((ssz = read(fd, buf, sizeof(buf))) > 0) {
-		pp = realloc(p, sz + ssz + 1);
-		if (NULL == pp) {
-			perror(NULL);
-			goto err;
-		}
-		p = pp;
+		p = realloc(p, sz + ssz + 1);
+		if (NULL == p)
+			err(1, NULL);
 		memcpy(&p[sz], buf, ssz);
 		p[sz + ssz] = '\0';
 		sz += ssz;
 	}
-	if (ssz < 0) {
-		perror(name);
-		goto err;
-	}
+	if (ssz < 0)
+		err(1, "%s", name);
 	close(fd);
 	fd = -1;
 
-	/* Parse as an iCalendar. */
-	if (NULL == (ical = ical_parse(name, p, sz))) 
-		goto err;
+	/* Try to parse as an iCalendar. */
+
+	if ((ical = ical_parse(name, p, sz)) == NULL) 
+		errx(1, "%s: not an iCalendar file", name);
 
 	ical_free(ical);
-	return(p);
-err:
-	ical_free(ical);
-	free(p);
-	if (-1 != fd)
-		close(fd);
-	return(NULL);
+	return p;
 }
 
 /*
@@ -150,7 +140,7 @@ err:
  * (as usual).
  * Both the user and realm strings must be non-empty.
  */
-static int
+static void
 gethash(int new, char *digest, 
 	const char *user, const char *realm)
 {
@@ -181,18 +171,16 @@ gethash(int new, char *digest,
 		sizeof(pbuf), RPP_REQUIRE_TTY);
 
 	if (pp == NULL) {
-		warnx("unable to read passphrase");
 		explicit_bzero(pbuf, sizeof(pbuf));
-		return 0;
+		errx(1, "unable to read passphrase");
 	} else if (pbuf[0] == '\0') {
 		explicit_bzero(pbuf, sizeof(pbuf));
-		return 0;
+		exit(1);
 	}
 
 	if (strlen(pbuf) < 6) {
-		warnx("come on: more than five letters");
 		explicit_bzero(pbuf, sizeof(pbuf));
-		return 0;
+		errx(1, "come on: more than five letters");
 	}
 
 	/* Hash according to RFC 2069's HA1. */
@@ -205,15 +193,12 @@ gethash(int new, char *digest,
 	MD5Update(&ctx, (const uint8_t *)pbuf, strlen(pbuf));
 	MD5Final(ha, &ctx);
 
+	explicit_bzero(pbuf, sizeof(pbuf));
+
 	/* Convert to hex format. */
 
 	for (i = 0; i < MD5_DIGEST_LENGTH; i++) 
 		snprintf(&digest[i * 2], 3, "%02x", ha[i]);
-
-	/* Zero the password buffer. */
-
-	explicit_bzero(pbuf, sizeof(pbuf));
-	return 1;
 }
 
 int
@@ -224,12 +209,13 @@ main(int argc, char *argv[])
 			 drep[MD5_DIGEST_LENGTH * 2 + 1],
 			 dold[MD5_DIGEST_LENGTH * 2 + 1];
 	const char	*realm = KREALM, *altuser = NULL, 
-	      		*dir = CALPREFIX, *email = NULL, *coln, *uid;
+	      		*dir = CALPREFIX, *email = NULL, 
+			*coln = NULL, *uid;
 	size_t		 i, sz;
 	uid_t		 euid = geteuid();
 	gid_t		 egid = getegid();
 	struct prncpl	*p = NULL;
-	struct coln	*col = NULL;
+	struct coln	*col;
 	char		*user = NULL, *emailp = NULL, *res;
 
 #if !HAVE_ARC4RANDOM
@@ -284,8 +270,21 @@ main(int argc, char *argv[])
 	if (create)
 		passwd = 1;
 
+	/* Safety: check collection name. */
+
 	if (coln != NULL && !check_safe_string(coln))
 		errx(1, "%s: unsafe collection name", coln);
+
+	/* Safety: check resource names. */
+
+	for (i = 0; i < (size_t)argc; i++) {
+		if ((uid = strrchr(argv[i], '/')) == NULL)
+			uid = argv[i];
+		else
+			uid++;
+		if (!check_safe_string(uid))
+			errx(1, "%s: unsafe resource name", argv[i]);
+	}
 
 	/* 
 	 * Assign our principal name.
@@ -304,10 +303,10 @@ main(int argc, char *argv[])
 	if (user == NULL)
 		err(1, NULL);
 	
-	if (!check_safe_string(user)) {
-		warnx("%s: unsafe principal name", user);
-		goto out;
-	}
+	/* Safety: check user name. */
+
+	if (!check_safe_string(user))
+		errx(1, "%s: unsafe principal name", user);
 
 	/* 
 	 * If we're not creating a new entry and we're setting our own
@@ -315,29 +314,23 @@ main(int argc, char *argv[])
 	 */
 
 	if (!create && altuser == NULL)
-		if (!gethash(0, dold, user, realm))
-			goto out;
+		gethash(0, dold, user, realm);
 
 	/* If we're going to set our password, hash it now. */
+
 	if (passwd) {
-		if (!gethash(1, dnew, user, realm)) 
-			goto out;
-		if (!gethash(2, drep, user, realm)) 
-			goto out;
-		if (memcmp(dnew, drep, sizeof(dnew))) {
-			warnx("passwords do not match");
-			goto out;
-		}
+		gethash(1, dnew, user, realm);
+		gethash(2, drep, user, realm);
+		if (memcmp(dnew, drep, sizeof(dnew)))
+			errx(1, "passwords do not match");
 	}
 
 	/* Regain privileges. */
-	if (-1 == setgid(egid)) {
-		kerr("setgid");
-		goto out;
-	} else if (-1 == setuid(euid)) {
-		kerr("setuid");
-		goto out;
-	}
+
+	if (setgid(egid) == -1)
+		err(1, "setgid");
+	if (setuid(euid) == -1) 
+		err(1, "setuid");
 
 	/*
 	 * Open the database file itself.
@@ -345,169 +338,174 @@ main(int argc, char *argv[])
 	 * regained effective credentials.
 	 * We only try to create the database if "create" is specified.
 	 */
-	if ( ! db_init(dir, create))
-		goto out;
+
+	if (!db_init(dir, create))
+		errx(1, "failed to open database");
 
 	/* Now drop privileges entirely! */
-	if (-1 == setgid(getgid())) {
-		kerr("setgid");
-		goto out;
-	} else if (-1 == setuid(getuid())) {
-		kerr("setuid");
-		goto out;
-	}
+
+	if (setgid(getgid()) == -1)
+		err(1, "setgid");
+	if (setuid(getuid()) == -1)
+		err(1, "setuid");
 
 	/* 
 	 * Check security of privileged operations.
-	 * This will also create the database schema if the database has
-	 * been created with "create" but doesn't exist yet.
+	 * This will also create the database if the database has been
+	 * created with "create" but doesn't exist yet.
 	 */
-	if (create || NULL != altuser) {
-		if (0 == (rc = db_owner_check_or_set(getuid()))) {
-			fprintf(stderr, "password file owner "
-				"must match the real user\n");
-			goto out;
-		} else if (rc < 0)
-			goto out;
+
+	if (create || altuser != NULL) {
+		if ((c = db_owner_check_or_set(getuid())) == 0)
+			errx(1, "db owner does not match real user");
+		else if (c < 0)
+			errx(1, "failed check or set db owner");
 	}
 
 	/* Now either create or update the principal. */
+
 	if (create) {
-		if (NULL == email) {
+		/*
+		 * Create e-mail by having our user followed by @
+		 * followed by the system hostname.
+		 * XXX: check if 256 for hostname is insufficient.
+		 */
+
+		if (email == NULL) {
 			sz = strlen(user) + 256 /* max hostname */ + 2;
-			emailp = malloc(sz);
-			if (NULL == emailp) {
-				kerr(NULL);
-				goto out;
-			}
+			if ((emailp = malloc(sz)) == NULL)
+				err(1, NULL);
 			strlcpy(emailp, user, sz);
 			strlcat(emailp, "@", sz);
-			gethostname(emailp + strlen(user) + 1, 
-				sz - strlen(user) - 1);
+			gethostname
+				(emailp + strlen(user) + 1, 
+				 sz - strlen(user) - 1);
 			email = emailp;
 		}
+
 		c = db_prncpl_new(user, dnew, email, 
-			NULL == coln ? "calendar" : coln);
-		if (0 == c) {
-			fprintf(stderr, "%s: principal exists\n", user);
-			goto out;
-		} else if (c < 0)
-			goto out;
+			coln == NULL ? "calendar" : coln);
+		if (c == 0)
+			errx(1, "%s: principal already exists", user);
+		else if (c < 0)
+			errx(1, "failed to create principal");
+
 		printf("principal created: %s\n", user);
-	} else if ((c = db_prncpl_load(&p, user)) > 0) {
-		if (NULL != coln) {
-			rc = db_collection_new(coln, p);
-			if (rc > 0)
+	} else {
+		if ((c = db_prncpl_load(&p, user)) == 0)
+			errx(1, "%s: principal does not exist", user);
+		else if (c < 0)
+			errx(1, "failed to load principal");
+
+		/* 
+		 * If we have a new collection, create it.
+		 * Don't report if it already exists.
+		 */
+
+		if (coln != NULL) {
+			if ((c = db_collection_new(coln, p)) < 0)
+				errx(1, "failed to create collection");
+			else if (c > 0)
 				printf("collection added: %s\n", coln);
-			else if (rc < 0)
-				goto out;
 		}
-		if (NULL == altuser)
-			if (memcmp(p->hash, dold, sizeof(dold))) {
-				fprintf(stderr, "password mismatch\n");
-				goto out;
-			}
-		if (NULL != email) {
+
+		/* Does the hash match what we gave? */
+
+		if (altuser == NULL)
+			if (memcmp(p->hash, dold, sizeof(dold)))
+				errx(1, "password mismatch");
+
+		/* Set new e-mail, if applicable. */
+
+		if (email != NULL) {
 			free(p->email);
-			p->email = strdup(email);
-			if (NULL == p->email) {
-				kerr(NULL);
-				goto out;
-			}
+			if ((p->email = strdup(email)) == NULL)
+				err(1, NULL);
 		}
+
+		/* Set new password (hash), if applicable. */
+
 		if (passwd) {
 			free(p->hash);
-			p->hash = strdup(dnew);
-			if (NULL == p->hash) {
-				kerr(NULL);
-				goto out;
-			}
+			if ((p->hash = strdup(dnew)) == NULL)
+				err(1, NULL);
 		}
-		if ( ! db_prncpl_update(p))
-			goto out;
-		printf("user modified: %s\n", user);
-	} else if (0 == c) {
-		fprintf(stderr, "%s: does not "
-			"exist, use -C to add\n", user);
-		goto out;
+
+		if ((c = db_prncpl_update(p)) == 0)
+			errx(1, "%s: e-mail already exists", p->email);
+		else if (c < 0)
+			errx(1, "failed to update principal");
+
+		printf("principal updated: %s\n", user);
 	}
 
-	/*
-	 * Success.
-	 * If we have no resources on the command line, exit.
-	 */
-	if (0 == argc) {
-		rc = 1;
+	/* If we have no resources on the command line, exit. */
+
+	if (argc == 0)
 		goto out;
-	}
 
 	/*
-	 * We need to laod some resources.
-	 * Now re-load our principal, which will have its new
-	 * directories contain therein.
+	 * We need to load some resources.
+	 * First re-load our principal, which will have its new
+	 * directories contained therein.
 	 */
+
 	prncpl_free(p);
 	p = NULL;
-	if (0 == (c = db_prncpl_load(&p, user))) {
-		fprintf(stderr, "user disappeared: %s\n", user);
-		goto out;
-	} else if (c < 0)
-		goto out;
+
+	if ((c = db_prncpl_load(&p, user)) == 0)
+		errx(1, "%s: principal disappeared!?", user);
+	else if (c < 0)
+		errx(1, "failed to re-load principal");
 
 	/* 
 	 * Search for the new collection, defaulting to the standard
 	 * calendar name if we didn't provide on.
 	 */
-	if (NULL == coln)
+
+	if (coln == NULL)
 		coln = "calendar";
+
 	for (col = NULL, i = 0; i < p->colsz; i++) 
-		if (0 == strcmp(p->cols[i].url, coln)) {
+		if (strcmp(p->cols[i].url, coln) == 0) {
 			col = &p->cols[i];
 			break;
 		}
 
-	if (NULL == col) {
-		fprintf(stderr, "collection "
-			"disappeared: %s\n", coln);
-		goto out;
-	}
+	if (col == NULL)
+		errx(1, "%s: collection disappeared!?", coln);
 
 	/*
 	 * Now go through each file on the command line, load it, then
 	 * push it into the collection.
 	 */
+
 	for (i = 0; i < (size_t)argc; i++) {
 		res = read_whole_file(argv[i]);
-		if (NULL == res)
-			goto out;
-		if (NULL == (uid = strrchr(argv[i], '/')))
+		assert(res != NULL);
+
+		/* Get file-name component. */
+
+		if ((uid = strrchr(argv[i], '/')) == NULL)
 			uid = argv[i];
 		else
 			uid++;
 
-		if ( ! check_safe_string(uid)) {
-			fprintf(stderr, "unsafe resource name\n");
-			free(res);
-			goto out;
-		}
-
 		rc = db_resource_new(res, uid, col->id);
 		free(res);
-		if (0 == rc) {
-			fprintf(stderr, "resource "
-				"exists: %s\n", argv[i]);
-			goto out;
-		} else if (rc < 0)
-			goto out;
+		if (rc == 0)
+			errx(1, "%s: resource exists", argv[i]);
+		else if (rc < 0)
+			errx(1, "failed to create resource");
+
 		printf("resource added: %s\n", argv[i]);
 	}
 
-	rc = 1;
 out:
 	prncpl_free(p);
 	free(user);
 	free(emailp);
-	return rc ? EXIT_SUCCESS : EXIT_FAILURE;
+	return 0;
 usage:
 	fprintf(stderr, "usage: %s "
 		"[-Cnv] "
@@ -516,5 +514,5 @@ usage:
 		"[-f caldir] "
 		"[-u principal] [resource...]\n", 
 		getprogname());
-	return(EXIT_FAILURE);
+	return 1;
 }
